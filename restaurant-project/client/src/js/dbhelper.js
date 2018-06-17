f919d4315450d5df4d68db067a4d3bfe8e77e8d9
Oliver Erxleben <oliver.erxleben@me.com>
2018-06-17 22:51:25 +0200

    fetch by id

--- restaurant-project/client/src/js/dbhelper.js
+++ restaurant-project/client/src/js/dbhelper.js
@@ -29,41 +29,16 @@ class DBHelper {
     .then( (json) => {
       callback(null, json);
     }) 
-
-    // let xhr = new XMLHttpRequest();
-    // xhr.open('GET', DBHelper.DATABASE_URL);
-    // xhr.onload = () => {
-    //   if (xhr.status === 200) { // Got a success response from server!
-        
-    //     const json = JSON.parse(xhr.responseText);
-    //     console.log(json);
-        
-    //     callback(null, json);
-    //   } else { // Oops!. Got an error from server.
-    //     const error = (`Request failed. Returned status of ${xhr.status}`);
-    //     callback(error, null);
-    //   }
-    // };
-    // xhr.send();
   }
 
   /**
    * Fetch a restaurant by its ID.
    */
   static fetchRestaurantById(id, callback) {
-    // fetch all restaurants with proper error handling.
-    DBHelper.fetchRestaurants((error, restaurants) => {
-      if (error) {
-        callback(error, null);
-      } else {
-        const restaurant = restaurants.find(r => r.id == id);
-        if (restaurant) { // Got the restaurant
-          callback(null, restaurant);
-        } else { // Restaurant does not exist in the database
-          callback('Restaurant does not exist', null);
-        }
-      }
-    });
+
+    fetch(DBHelper.DATABASE_URL + id)
+      .then( (response) => response.json())
+      .then( (json) => callback(null, json))
   }
 
   /**
